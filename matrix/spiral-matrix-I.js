3d74fbed434cc5fe6c37e9cd54c53a42b9b9510b
Anthonia Alexis Asikpo <42866195+devgemmy@users.noreply.github.com>
2021-09-16 10:08:43 +0100

    Update and rename longest-turbulent-subarray.js to matrix/spiral-matrix-I.js

---
+++ matrix/spiral-matrix-I.js
@@ -0,0 +1,10 @@
+const spiralOrder = (matrix) => {
+    let m = matrix.length,
+        n = 0;
+    
+    for (let i = 0; i < matrix.length; i++) {
+        n = matrix[i].length;
+    }
+    
+    console.log(m, n)
+};
