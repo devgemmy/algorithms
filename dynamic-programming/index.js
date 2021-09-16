5fda85c2b16a2ad9b7c8dd653692909eb6d7ca4e
Anthonia Alexis Asikpo <42866195+devgemmy@users.noreply.github.com>
2021-09-16 10:10:54 +0100

    Update index.js

--- dynamic-programming/index.js
+++ dynamic-programming/index.js
@@ -1,6 +1,6 @@
 /* 
 
 Definition:
-
+Using the algorithmic technique of solving a problem by breaking it down into smaller problems knowing that the optimal solution of each small problem adds up to the ultimate solution of the whole problem.
 
 */
