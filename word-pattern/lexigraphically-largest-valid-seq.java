df15587840593efcea6203440d5511636b370ffe
Anthonia Alexis Asikpo <42866195+devgemmy@users.noreply.github.com>
2025-02-16 22:54:59 +0000

    Updated lexigraphically-largest-valid-seq.java

--- word-pattern/lexigraphically-largest-valid-seq.java
+++ word-pattern/lexigraphically-largest-valid-seq.java
@@ -10,35 +10,51 @@ class Solution {
     }
 
     private boolean backtrack(int[] result, boolean[] used, int n, int index) {
-        if (index = result.length) {
+        if (index == result.length) {
             res = result.clone();
             return true;
         }
 
-        while(index < result.length && result[index] != 0)
-            index++;
+        // while(index < result.length && result[index] != 0)
+        //     index++;
+
+        if (result[index] != 0) return backtrack(result, used, n, index + 1); // Skip occupied
         
         for (int i = n; i >= 1; i--) {
             if (used[i]) continue;
 
-            if (i == 1) {
-                result[index] = 1;
-                used[1] = true;
-                if(backtrack(result, used, n, index + 1)) return true;
+            int trace = (i == 1) ? index : index + i;
+            if (trace < result.length && result[trace] == 0) {
+                result[index] = i;
+                result[trace] = i;
+                used[i] = true;
+
+                if (backtrack (result, used, n, index + 1)) return true;
+
                 result[index] = 0;
-                used[1] = false;
-            } else {
-                if (index + i < result.length && result[index + i] == 0) {
-                    result[index] = i;
-                    result[index + i] = i;
-                    used[i] = true;
-                    if(backtrack(result, used, n, index + 1)) return true;
-                    result[index] = 0;
-                    result[index + 1] = 0;
-                    used[i] = false;
-                }
+                result[trace] = 0;
+                used[i] = false;
             }
+
+            // if (i == 1) {
+            //     result[index] = 1;
+            //     used[1] = true;
+            //     if(backtrack(result, used, n, index + 1)) return true;
+            //     result[index] = 0;
+            //     used[1] = false;
+            // } else {
+            //     if (index + i < result.length && result[index + i] == 0) {
+            //         result[index] = i;
+            //         result[index + i] = i;
+            //         used[i] = true;
+            //         if(backtrack(result, used, n, index + 1)) return true;
+            //         result[index] = 0;
+            //         result[index + 1] = 0;
+            //         used[i] = false;
+            //     }
+            // }
         }
+
         return false;
     }
 }
