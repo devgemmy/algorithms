d84cf2e83f35fa43a2479ec8472711bdaaf52467
Anthonia Alexis Asikpo <42866195+devgemmy@users.noreply.github.com>
2025-02-16 22:40:45 +0000

    updated palindrome code for integers

--- word-pattern/palindrome.java
+++ word-pattern/palindrome.java
@@ -1,5 +1,6 @@
 import java.util.Arrays;
 
+// For String
 public class Main {
     public static String breakPalindrome(String palindrome) {
         int longLength = palindrome.length();
@@ -18,6 +19,28 @@ public class Main {
         
         palindromeArr[longLength - 1] = 'b';
         return new String(palindromeArr);
+    }
+}
+
+// For Integer
+class Solution {
+    public boolean isPalindrome(int x) {
+        if (x < 0)
+           return false;
+
+        int z = x;
+        int rev = 0;
+        while (x > 0){
+            int lst = x % 10;
+            rev = rev * 10 + lst;
+            x = x / 10;
+        }
+
+        if(rev == z){
+            return true;
+        } else{
+            return false;
+        }
     }
 }
 
