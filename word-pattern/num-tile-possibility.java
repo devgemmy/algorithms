ddb9f21b28e70dbeac6a8f63efc911ab5dea82a7
Anthonia Alexis Asikpo <42866195+devgemmy@users.noreply.github.com>
2025-02-17 23:24:43 +0000

    Create num-tile-possibility.java

---
+++ word-pattern/num-tile-possibility.java
@@ -0,0 +1,25 @@
+class Solution {
+    /// permutation + bactracking
+    public int numTilePossibilities(String tiles) {
+        int[] freq = new int[26];
+        for(char c: tiles.toCharArray()){
+            freq[c - 'A']++;
+        }
+
+        return backtrack(freq);
+    }
+        
+    private int backtrack(int[] freq){
+        int count = 0;
+        for(int i=0; i<26; i++){
+            if(freq[i] != 0){
+                count++;
+                freq[i]--;
+                count += backtrack(freq);
+                freq[i]++;
+            } else continue;
+        }
+        
+        return count;
+    }
+}
